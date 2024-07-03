cd382e2056c29faca9148dfa6f37e16729543311
rmkofficial <kmert0@outlook.com>
2024-07-03 10:15:56 +0300

    Add react-icons package and update dependencies

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { IoMdRemoveCircleOutline } from "react-icons/io";
+import { FaCheck } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 
 function Todo() {
   return (
-    <div>
-        Ilk todo
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        border: "1px solid #ddd",
+        borderRadius: "5px",
+        padding: "16px",
+        marginTop: "25px",
+      }}
+    >
+      <div>Ilk todo</div>
+      <div>
+        <IoMdRemoveCircleOutline
+          className="icons"
+          style={{ marginRight: "8px" }}
+        />
+        <FaEdit className="icons" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
